b7b724e8cc6da312308e2ef8d5811702ed104482
git12nazneen <lipinazneen65@gmail.com>
2024-10-04 01:42:29 +0600

    routingg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Landing from "../components/modules/home/Landing";
-import RecentsPost from "../components/modules/home/RecentsPost";
+import Landing from "@/src/components/modules/home/Landing";
+import RecentsPost from "@/src/components/modules/home/RecentsPost";
 
 export default function Home() {
   return (
